748853825127ac7282c3aa97692046cb64067459
Saeed Khatami <69420353+0xb4dc0d3x@users.noreply.github.com>
2022-02-28 11:58:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,27 @@ from tkinter import *
 
 root = Tk()
 root.withdraw()
-waitTime = 5
-folder = filedialog.askdirectory()
+isOkay = False
+while isOkay == False :
+    try:
+        inputN = input("How long should I wait? >>")
+        waitTime = int(inputN)
+        isOkay = True
+    except:
+        isOkay = False
+def Write():
+    f = open("config.txt", "a")
+    f.write(filedialog.askdirectory())
+    f.close()
+def Invalid():
+    os.remove("config.txt") 
+    Write()
+    return open("config.txt", "r").read()
+try:
+    folder = open("config.txt", "r").read()
+except:
+    Write()
+    folder = open("config.txt", "r").read()
 
 
 def wall(image_path):
@@ -19,17 +38,21 @@ def wall(image_path):
     SPIF_UPDATEINIFILE = 0x0001
     SPIF_SENDWININICHANGE = 0x0002
     image = image_path
-    image_path2 = os.path.join( folder, image)
+    image_path2 = os.path.join(folder, image)
     user32 = ctypes.WinDLL('user32')
     SystemParametersInfo = user32.SystemParametersInfoW
-    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0,image_path2, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, image_path2, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+
 
 def main():
-    image_path = os.path.join( folder)
-    images = os.listdir(image_path)
+    image_path = os.path.join(folder)
+    try :
+        images = os.listdir(image_path)
+    except :
+       images = os.listdir(Invalid())
     for image in images[:]:
         if not image.endswith(("png", "jpg", "ico", "jpeg")):
-            images.remove(image)
+             images.remove(image)
     counter = 0
     while counter <= len(images):
         try:
@@ -46,5 +69,6 @@ def main():
             print(e)
             time.sleep(waitTime)
             continue
+
 
 main()
